dadde5feb1a6f37a74430b7c41b95e304c8f8d0b
zh0glikk <zhogliksasha3434@gmail.com>
2023-12-02 16:16:53 +0200

    small fix

--- internal/arbitrage/transactor/implementations/swap_transactor.go
+++ internal/arbitrage/transactor/implementations/swap_transactor.go
@@ -44,29 +44,38 @@ func (i *SwapTransactor) PrepareTemplates(data *simulation_models.PrepareTemplat
 		if err != nil {
 			return err
 		}
+		log.Info(fmt.Sprintf("packing swapV2 input template: %s", i.purchaseTemplate))
+
 	} else {
 		i.purchaseTemplate, err = packer.PackerObj.PackSwapV3Template(data.InputPair, data.InputToken, data.OutputToken, data.Contract)
 		if err != nil {
 			return err
 		}
+		log.Info(fmt.Sprintf("packing swapV3 input template: %s", i.purchaseTemplate))
+
 	}
 
 	if data.OutputPairVersion == 2 {
-		i.purchaseTemplate, err = packer.PackerObj.PackSwapV2Template(data.OutputPair, data.OutputToken, data.InputToken)
+		i.sellTemplate, err = packer.PackerObj.PackSwapV2Template(data.OutputPair, data.OutputToken, data.InputToken)
 		if err != nil {
 			return err
 		}
+		log.Info(fmt.Sprintf("packing swapV2 output template: %s", i.sellTemplate))
+
 	} else {
-		i.purchaseTemplate, err = packer.PackerObj.PackSwapV3Template(data.OutputPair, data.OutputToken, data.InputToken, data.Contract)
+		i.sellTemplate, err = packer.PackerObj.PackSwapV3Template(data.OutputPair, data.OutputToken, data.InputToken, data.Contract)
 		if err != nil {
 			return err
 		}
+		log.Info(fmt.Sprintf("packing swapV3 output template: %s", i.sellTemplate))
+
 	}
 
 	return nil
 }
 
 func (i *SwapTransactor) PackPurchase(data *simulation_models.PackFrontDTO) ([]byte, error) {
+	log.Info(fmt.Sprintf("purchase %d %v", i.purchaseType, data.Value))
 	if data.Value == nil {
 		return nil, nil
 	}
@@ -78,6 +87,8 @@ func (i *SwapTransactor) PackPurchase(data *simulation_models.PackFrontDTO) ([]b
 }
 
 func (i *SwapTransactor) PackSell(data *simulation_models.PackBackDTO) ([]byte, error) {
+	log.Info(fmt.Sprintf("sell %d %v", i.sellType, data.Value))
+
 	if data.Value == nil {
 		return nil, nil
 	}
